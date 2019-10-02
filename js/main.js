ba2a793b8693d55c659eeb3ad9f043f32f959669
Shahriar Saleque <salequeshahriar@gmail.com>
2019-10-02 19:06:05 +0600

    Completed the App

--- js/main.js
+++ js/main.js
@@ -5,18 +5,27 @@ const result = document.getElementById("result");
 const restart = document.getElementById("restart");
 const modal = document.querySelector(".modal");
 
+//Object to keep all the scores
+const scoreboard = {
+  player: 0,
+  computer: 0
+};
+
 //Add Event Listeners
 choices.forEach(choice => {
-  choice.addEventListener("click", getPlayer);
+  choice.addEventListener("click", play);
 });
 
 //Functions
-function getPlayer(e) {
+function play(e) {
+  restart.style.display = "inline-block";
   const playerChoice = e.target.id;
   const computerChoice = getComputerChoice();
   //Decide who the winner is
   const winner = getWinner(playerChoice, computerChoice);
   console.log(playerChoice, computerChoice, winner);
+  //Display winner as modal on webpage
+  showWinner(winner, playerChoice, computerChoice);
 }
 
 function getComputerChoice() {
@@ -27,7 +36,7 @@ function getComputerChoice() {
   } else if (random <= 0.67) {
     return "paper";
   } else {
-    return "scissor";
+    return "scissors";
   }
 }
 
@@ -53,4 +62,45 @@ function getWinner(p, c) {
       return "player";
     }
   }
+}
+
+function showWinner(w, p, c) {
+  if (w === "computer") {
+    scoreboard.computer++;
+    result.innerHTML = `
+ <h1 class="text-lose">You Lose</h1>
+ <i class="fas fa-hand-${c} fa-10x"></i>
+ <p>Computer Chose ${c}</p>
+ 
+ `;
+  } else if (w === "player") {
+    scoreboard.player++;
+    result.innerHTML = `
+     <h1 class="text-win">You Win</h1>
+     <i class="fas fa-hand-${c} fa-10x"></i>
+     <p>Computer Chose ${c}</p>
+     
+     `;
+  } else {
+    result.innerHTML = `
+    <h1>Its a Draw</h1>
+    <i class="fas fa-hand-${c} fa-10x"></i>
+    <p>Computer Chose the same ${c} as You</p>
+    
+    `;
+  }
+
+  //Dynamically change the scoreboard
+  score.innerHTML = `
+  <p>Player: ${scoreboard.player}</p>
+  <p>Computer: ${scoreboard.computer}</p>
+  
+  `;
+
+  modal.style.display = "inline-block";
+
+  //Clear the resulting modal
+  setTimeout(function() {
+    modal.style.display = "none";
+  }, 3000);
 }
